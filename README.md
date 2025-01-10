8f6e78ff40bf915e0e00d1dd59edc4207e81a82d
lifeOfLearn <java987654251@gmail.com>
2025-01-10 17:51:04 +0800

    test

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # SpringWebMVC
+1.練習使用ecplise 建立Maven專案
+2.載入Spring web MVC套件
+3.data目錄中記錄流程與xml檔案
+xml檔案中註解註記spring官網哪裡
+4.word檔案目前新增
+	ApplicationContext
+	ContextLoaderListener
+	DispatcherServlet
+		-說明流程過程
+	簡單的tomcat啟動後的載入
---
+++ src/main/java/owner/exercise/controller/HelloController.java
@@ -0,0 +1,14 @@
+package owner.exercise.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class HelloController {
+	
+	@GetMapping("hello")
+	public ModelAndView hello() {
+		return new ModelAndView("helloViewName");
+	}
+}
